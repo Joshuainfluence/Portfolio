e90092a166e06e7fea5a3f183fae29d745b507ff
unknown <joshuajulius2030@gmail.com>
2024-04-05 08:38:04 +0100

    succesful

---
+++ app.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+// link https://github.com/mattboldt/typed.js
