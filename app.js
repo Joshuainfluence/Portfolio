d1b8d901a315cf7bb52ffaec96fb5d8dd271af72
unknown <joshuajulius2030@gmail.com>
2024-04-06 18:47:53 +0100

    initialized

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    strings: ["Full Stack Developer", "Mobile App Developer", "Cybersecurity Ethusiast", "Computer Science Student", "Youtuber"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
